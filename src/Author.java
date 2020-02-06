bf235431b496a10e61f4c5cd64a22679ae61103a
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-06 15:15:56 -0600

    completed easy_fix exercise

--- src/Author.java
+++ src/Author.java
@@ -1,8 +1,11 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
+
 import java.util.ArrayList;
 import java.util.List;
+
+@SuppressWarnings({"unchecked", "deprecated"})
 
 public class Author extends Person {
     private List books;
@@ -28,7 +31,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-//    @Override
+    //    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
